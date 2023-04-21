e5339d824d9395ffeb65c9fadab6ef88ffd60e29
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-04-21 15:58:29 +0800

    Header

---
+++ decryptor.py
@@ -0,0 +1,9 @@
+# Paulean Marguerette F. Parrish
+# BSCPE 1-4
+# Problem 2 of Assignment 2
+# The purpose of this program is to decrypt a string that has been encrypted
+
+import pyfiglet
+
+# Header for the activity
+print("Decryption".center(70, "="))
